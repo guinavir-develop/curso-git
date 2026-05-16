353532dbf4b03faf229a7db62293a23b3a4a38bf
Guilherme Martins <gui.navir2025@gmail.com>
2026-05-16 16:42:04 -0400

    :sparkles: feat: ajuste para funcionamento de impressão total

--- code.c
+++ code.c
@@ -4,9 +4,9 @@ int main ()
 {
     int limite = 10;
 
-    printf("contamdo numeros pares ate %d:\n", limite);
+    printf("contando numeros ate %d:\n", limite);
 
-    for ( int i = 0 ; i <= limite ; i+= 2)
+    for ( int i = 0 ; i <= limite ; i++)
     {
         
         printf("> %d\n",i);
